5538d6f86296878388772a231ad74d952d01b703
Arron Alsop <alsop.arron@gmail.com>
2023-11-27 12:15:09 -0800

    'wip'

--- api/server.js
+++ api/server.js
@@ -5,6 +5,34 @@ const Users = require('./users/model')
 const server = express()
 server.use(express.json())
 
+
+server.put('/api/users/:id', async (req, res) => {
+
+    try {
+        const possibleUser = await Users.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user',
+                })
+            } else {
+               const changingUser = await Users.update(
+                req.params.id, 
+                req.body)
+               res.status(200).json(changingUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+             message: 'error updating user', 
+             err: err.message,
+             stack: err.stack,
+    })
+    }
+})
+
 server.delete('	/api/users/:id', async (req, res) => {
   try {
     const possibleUser = await Users.findById(req.params.id)
@@ -60,26 +88,6 @@ server.get('/api/users/:id', (req, res) => {
             res.status(500).json({ message: "The user information could not be retrieved" })
         })
 })
-
-
-
-
-server.put('/api/users/:id', async (req, res) => {
-    const changes = req.body
-    const { id } = req.params
-
-    try {
-        const updatedUser = await Users.update(id, changes)
-        if (updatedUser) {
-            res.status(200).json(updatedUser)
-        } else {
-            res.status(404).json({ message: `user not found with id ${id}` })
-        }
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
-})
-
 
 
 server.use('*', (req, res) => {
