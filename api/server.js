5784d2f432dc0c5a75693ebe640a970eb2506761
Arron Alsop <alsop.arron@gmail.com>
2023-11-27 12:02:24 -0800

    'wip'

--- api/server.js
+++ api/server.js
@@ -5,6 +5,37 @@ const Users = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.delete('	/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await Users.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+        const deletedUser = await Users.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The user information could not be modified" })
+  }
+})
+
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    Users.insert(user)
+    if (!user.name || !user.bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user'})
+    } else {
+      Users.insert(user)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+    }  
+})
+
 server.get('/api/users', (req, res) => {
     Users.find()
         .then(users => {
@@ -32,21 +63,6 @@ server.get('/api/users/:id', (req, res) => {
 
 
 
-server.post('/api/users', (req, res) => {
-    const user = req.body
-    Users.insert(user)
-    if (!user.name || !user.bio) {
-        res.status(400).json({ message: 'Please provide name and bio for the user'})
-    } else {
-      Users.insert(user)
-        .then(newUser => {
-            res.status(201).json(newUser)
-        })
-        .catch(err => {
-            res.status(500).json({ message: "There was an error while saving the user to the database" })
-        })
-    }  
-})
 
 server.put('/api/users/:id', async (req, res) => {
     const changes = req.body
@@ -64,21 +80,7 @@ server.put('/api/users/:id', async (req, res) => {
     }
 })
 
-server.delete('	/api/users/:id', (req, res) => {
-    const { id } = req.params
 
-    Users.delete(id)
-      .then(deleted => {
-        if (deleted) {
-            res.status(200).json(deleted)
-        } else {
-            res.status(404).json({ message: `user not found with id ${id}`})
-        }
-      }) 
-      .catch(err => {
-        res.status(500).json({ error: err.message })
-      })
-})
 
 server.use('*', (req, res) => {
     res.status(404).json({
