2b50e81db9f8552bd7b9df1b4fba5753bcca3bca
Arron Alsop <alsop.arron@gmail.com>
2023-12-05 07:59:01 -0800

    'wip'

--- api/server.js
+++ api/server.js
@@ -20,7 +20,8 @@ server.put('/api/users/:id', async (req, res) => {
             } else {
                const changingUser = await Users.update(
                 req.params.id, 
-                req.body)
+                req.body
+                )
                res.status(200).json(changingUser)
             }
         }
@@ -43,14 +44,18 @@ server.delete('	/api/users/:id', async (req, res) => {
         res.status(200).json(deletedUser)
     }
   } catch (err) {
-    res.status(500).json({ message: "The user information could not be modified" })
+    res.status(500).json({ 
+        message: "The user information could not be modified",
+        error: err.message,
+        stack: err.stack 
+    })
   }
 })
 
 
 server.post('/api/users', (req, res) => {
     const user = req.body
-    Users.insert(user)
+    
     if (!user.name || !user.bio) {
         res.status(400).json({ message: 'Please provide name and bio for the user'})
     } else {
@@ -59,33 +64,45 @@ server.post('/api/users', (req, res) => {
             res.status(201).json(newUser)
         })
         .catch(err => {
-            res.status(500).json({ message: "There was an error while saving the user to the database" })
+            res.status(500).json({ 
+                message: "There was an error while saving the user to the database",
+                error: err.message,
+                stack: err.stack,
+            })
         })
     }  
-})
+})  
 
 server.get('/api/users', (req, res) => {
     Users.find()
         .then(users => {
-            res.status(200).json(users)
+            res.status(users)
         })
         .catch(err => {
-            res.status(500).json({ message: "The users information could not be retrieved"  })
+            res.status(500).json({ 
+                message: "The users information could not be retrieved",
+                error: err.message,
+                stack: err.stack,
+              })
         })
 })
 
 
 server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params
-
-    Users.findById(id) 
+     Users.findById(req.params.id) 
         .then(user => {
-            user
-             ? res.status(200).json(user)
-             : res.status(404).json({ message: "The user with the specified ID does not exist"  })
+           if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist"})
+           } else {
+            res.json(user)
+           }
         })
         .catch(err => {
-            res.status(500).json({ message: "The user information could not be retrieved" })
+            res.status(500).json({ 
+                message: "The user information could not be retrieved",
+                error: err.message,
+                stack: err.stack,
+             })
         })
 })
 
